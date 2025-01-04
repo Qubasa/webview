d0a54f42d24b529e239be5bed6295d63193cf527
Qubasa <consulting@qube.email>
2025-01-04 02:12:05 +0100

    flakify

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "A Nix-flake-based C/C++ development environment";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell.override
+          {
+            # Override stdenv in order to change compiler:
+            # stdenv = pkgs.clangStdenv;
+          }
+          {
+            packages = with pkgs; [
+              clang-tools
+              cmake
+              doxygen
+              pkg-config
+              gtk4
+              webkitgtk_6_0
+            ] ++ (if system == "aarch64-darwin" then [ ] else [ gdb ]);
+          };
+      });
+    };
+}
+
